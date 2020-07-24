9203559d0ee1fde48e9f61405c3553c9c5b6f5b1
crazynayan <nayan@crazyideas.co.in>
2020-07-24 16:24:40 +0530

    signup and login

--- dbschema.js
+++ dbschema.js
@@ -7,5 +7,13 @@ let db = {
       likeCount: 5,
       commentCount: 2
     }
+  ],
+  users: [
+    {
+      userId: "rDTHKYnR2HX3NqDKUhmVsu1JeTD3",
+      email: "user@example.com",
+      handle: "user",
+      createdAt: "2020-07-24T08:35:08.050Z"
+    }
   ]
 }
--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,26 @@
 const functions = require("firebase-functions")
 const admin = require("firebase-admin")
-
 admin.initializeApp()
+const db = admin.firestore()
+const app = require("express")()
 
-const express = require("express")
-const app = express()
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDqlmp_X8oqTeLx_bKQsEcNUK7QYSr_ick",
+  authDomain: "scream-tutorial.firebaseapp.com",
+  databaseURL: "https://scream-tutorial.firebaseio.com",
+  projectId: "scream-tutorial",
+  storageBucket: "scream-tutorial.appspot.com",
+  messagingSenderId: "281483852651",
+  appId: "1:281483852651:web:1302d196eeafe4b1f7ea2e",
+  measurementId: "G-0722ZW0SV5"
+}
+const firebase = require("firebase")
+firebase.initializeApp(firebaseConfig)
 
 app.get("/screams", async(request, response) => {
   try {
-    const snapshot = await admin.firestore().collection("screams").orderBy("createdAt", "desc").get()
+    const snapshot = await db.collection("screams").orderBy("createdAt", "desc").get()
     let screams = []
     snapshot.docs.forEach(doc => screams.push({screamId: doc.id, ...doc.data()}))
     response.send(screams)
@@ -25,11 +37,70 @@ app.post("/scream",async (request, response) => {
       userHandle: request.body.userHandle,
       createdAt: new Date().toISOString()
     }
-    const doc = await admin.firestore().collection("screams").add(newScream)
+    const doc = await db.collection("screams").add(newScream)
     response.send({"message": `document ${doc.id} created successfully`})
   } catch (error) {
     console.error(error)
     response.status(500).send({"error": "error in creating scream"})
+  }
+})
+
+app.post("/signup", async(request, response) => {
+  const newUser = {...request.body}
+  const emailRegEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  let errors = {}
+  if (newUser.email.trim() === "")
+    errors.email = "Must not be empty"
+  else if (!newUser.email.match(emailRegEx))
+    errors.email = "Must be a valid email address"
+  if (newUser.password.trim() === "")
+    errors.password = "Must not be empty"
+  if (newUser.password !== newUser.confirmPassword)
+    errors.confirmPassword = "Passwords must match"
+  if (newUser.handle.trim() === "")
+    errors.handle = "Must not be empty"
+  if (Object.keys(errors).length > 0)
+    response.status(400).send(errors)
+  try {
+    const doc = await db.doc(`/users/${newUser.handle}`).get()
+    if (doc.exists) {
+      response.status(400).send({handle: "this handle is already taken"})
+    }
+    const data = await firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password)
+    const token = await data.user.getIdToken()
+    const userCredentials = {
+      handle: newUser.handle,
+      email: newUser.email,
+      createdAt: new Date().toISOString(),
+      userId: data.user.uid
+    }
+    await db.doc(`/users/${newUser.handle}`).set(userCredentials)
+    response.status(201).send({token})
+  } catch (error) {
+    console.error(error)
+    if (error.code === "auth/email-already-in-use") {
+      response.status(400).send({"email": error.message})
+    }
+    response.status(500).send({"error": error.code})
+  }
+})
+
+app.post("/login", async(request, response) => {
+  const user = {...request.body}
+  let errors = {}
+  if (user.email.trim() === "")
+    errors.email = "Must not be empty"
+  if (user.password.trim() === "")
+    errors.password = "Must not be empty"
+  if (Object.keys(errors).length > 0)
+    response.send({errors})
+  try {
+    const data = await firebase.auth().signInWithEmailAndPassword(user.email, user.password)
+    const token = await data.user.getIdToken()
+    response.send({token})
+  } catch (error) {
+    console.error(error)
+    response.status(500).send({"error": error.code})
   }
 })
 

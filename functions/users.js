069f8a42d61344d2ff9571121234212ec12eebea
crazynayan <nayan@crazyideas.co.in>
2020-08-04 19:03:59 +0530

    addUserDetails will respond with updated user data

--- functions/users.js
+++ functions/users.js
@@ -5,41 +5,47 @@ firebase.initializeApp(firebaseConfig)
 const {uuid} = require("uuidv4")
 const emailRegEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
-exports.signup = async (request, response) => {
-  const newUser = {...request.body}
+exports.signup = (request, response) => {
+  const user = {...request.body}
   let errors = {}
-  if (newUser.email.trim() === "")
+  if (!user)
+    return response.status(400).send({email: "Invalid request, please try again"})
+  if (!user || user.email.trim() === "")
     errors.email = "must not be empty"
-  else if (!newUser.email.match(emailRegEx))
+  else if (!user.email.match(emailRegEx))
     errors.email = "must be a valid email address"
-  if (newUser.password.trim() === "")
+  if (user.password.trim() === "")
     errors.password = "must not be empty"
-  if (newUser.password !== newUser.confirmPassword)
-    errors.confirmPassword = "Passwords must match"
-  if (newUser.handle.trim() === "")
+  if (user.password.length < 6)
+    errors.password = "must be at least 6 characters"
+  if (user.password !== user.confirmPassword)
+    errors.confirmPassword = "passwords must match"
+  if (user.handle.trim() === "")
     errors.handle = "must not be empty"
   if (Object.keys(errors).length > 0)
     return response.status(400).send(errors)
-  try {
-    const doc = await db.doc(`/users/${newUser.handle}`).get()
-    if (doc.exists)
-      return response.status(400).send({handle: "this handle is already taken"})
-    const data = await firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password)
-    const token = await data.user.getIdToken()
-    await db.doc(`/users/${newUser.handle}`).set({
-      handle: newUser.handle,
-      email: newUser.email,
-      createdAt: new Date().toISOString(),
-      userId: data.user.uid,
-      imageUrl: getImageUrl("blank.jpg", "9f8fcdf9-9325-47c7-8b37-8807472dfe8b")
-    })
-    response.send({token})
-  } catch (error) {
-    console.error(error)
-    if (error.code === "auth/email-already-in-use")
-      return response.status(400).send({email: error.message})
-    response.status(500).send({general: "Something went wrong, please try again"})
-  }
+  return (async () => {
+    try {
+      const doc = await db.doc(`/users/${user.handle}`).get()
+      if (doc.exists)
+        return response.status(400).send({handle: "this handle is already taken"})
+      const data = await firebase.auth().createUserWithEmailAndPassword(user.email, user.password)
+      const token = await data.user.getIdToken()
+      await db.doc(`/users/${user.handle}`).set({
+        handle: user.handle,
+        email: user.email,
+        createdAt: new Date().toISOString(),
+        userId: data.user.uid,
+        imageUrl: getImageUrl("blank.jpg", "9f8fcdf9-9325-47c7-8b37-8807472dfe8b")
+      })
+      return response.send({token})
+    } catch (error) {
+      console.error(error)
+      if (error.code === "auth/email-already-in-use")
+        return response.status(400).send({email: error.message})
+      return response.status(500).send({email: "Something went wrong, please try again"})
+    }
+  })()
 }
 
 exports.login = (request, response) => {
@@ -53,7 +59,7 @@ exports.login = (request, response) => {
     if (!user.password || user.password.trim() === "")
       errors.password = "must not be empty"
   } else {
-    errors.general = "invalid request format"
+    errors.password = "invalid request format"
   }
   if (Object.keys(errors).length > 0)
     return response.status(400).send(errors)
@@ -63,22 +69,30 @@ exports.login = (request, response) => {
       const token = await data.user.getIdToken()
       return response.send({token})
     } catch (error) {
-      return response.status(403).send({general: "Wrong credentials, please try again"})
+      return response.status(403).send({password: "Wrong credentials, please try again"})
     }
   })()
 }
 
 exports.addUserDetails = async (request, response) => {
   let userDetails = {...request.body}
-  if (!userDetails.website.startsWith("http")) {
+  if (!userDetails.website || userDetails.website.trim() === "")
+    userDetails.website = ""
+  if (!userDetails.bio || userDetails.bio.trim() === "")
+    userDetails.bio = ""
+  if (!userDetails.location || userDetails.location.trim() === "")
+    userDetails.location = ""
+  if (!!userDetails.website && !userDetails.website.startsWith("http")) {
     userDetails.website = `http://${userDetails.website}`
   }
   try {
-    await db.doc(`/users/${request.user.handle}`).update(userDetails)
-    response.send({message: "user details updated successfully"})
+    const userRef = db.doc(`/users/${request.user.handle}`)
+    await userRef.update(userDetails)
+    const userDoc = await userRef.get()
+    return response.send({...userDoc.data()})
   } catch (error) {
     console.error(error)
-    response.status(500).send({error: error.code})
+    return response.status(500).send({error: error.code})
   }
 }
 
